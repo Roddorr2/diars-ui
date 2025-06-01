5ea40dc4d59ce69131e7edd3311d1159019656d8
Roddorr2 <rodricu0703@gmail.com>
2025-06-01 01:37:17 -0500

    Added dashboard component path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard.component";
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
   },
   {
-    path: 'login',
+    path: "login",
     component: LoginComponent,
   },
+  { path: "dashboard", component: DashboardComponent },
 ];
