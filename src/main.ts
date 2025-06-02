fd5a3f4647d1aae5ceb7f72f9857507a465ad3b4
Roddorr2 <rodricu0703@gmail.com>
2025-06-02 04:02:54 -0500

    Corrected the one thing that wouldn't let me view my product list

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app/app.routes";
+import { authInterceptor } from "./app/auth/auth.interceptor";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 }).catch((err) => console.error(err));
