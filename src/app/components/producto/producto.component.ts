e4d773332af45481a328356f86b088288ec0e593
Roddorr2 <rodricu0703@gmail.com>
2025-06-02 02:50:19 -0500

    Implemented all methods and validations for frontend

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ProductoService } from "./../../services/producto.service";
 import { Producto } from "../../models/producto.model";
 import { Subcategoria } from "./../../models/subcategoria.model";
 import { SubcategoriaService } from "./../../services/subcategoria.service";
+import { Router } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CurrencyPipe } from "@angular/common";
+
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-producto",
-  imports: [],
+  imports: [ReactiveFormsModule, CurrencyPipe],
   templateUrl: "./producto.component.html",
   styleUrl: "./producto.component.css",
 })
@@ -15,6 +20,7 @@ export class ProductoComponent implements OnInit {
   productos: Producto[] = [];
   subcategorias: Subcategoria[] = [];
   formularioProducto!: FormGroup;
+  router = inject(Router);
 
   modoEdicion: boolean = false;
   productoSeleccionadoId?: number;
@@ -71,6 +77,16 @@ export class ProductoComponent implements OnInit {
       this.cargarProductos();
       this.cancelar();
     });
+
+    Swal.fire({
+      title: "¡Registrado!",
+      text: "El producto se registró correctamente.",
+      icon: "success",
+      timer: 2000,
+      showConfirmButton: false,
+      toast: true,
+      position: "top-end",
+    });
   }
 
   editar(producto: Producto): void {
@@ -89,5 +105,86 @@ export class ProductoComponent implements OnInit {
       unidadMedida: producto.unidadMedida,
       estado: producto.estado,
     });
+  }
+
+  actualizar(): void {
+    if (this.formularioProducto.invalid || this.productoSeleccionadoId == null)
+      return;
+
+    const producto: Producto = {
+      id: this.productoSeleccionadoId,
+      ...this.formularioProducto.value,
+      subcategoria: {
+        id: this.formularioProducto.value.subcategoria,
+      } as Subcategoria,
+    };
+
+    this.productoService
+      .modificar(this.productoSeleccionadoId, producto)
+      .subscribe(() => {
+        this.cargarProductos();
+        this.cancelar();
+      });
+
+    Swal.fire({
+      title: "¡Modificado!",
+      text: "El producto se modificó correctamente.",
+      icon: "success",
+      timer: 2000,
+      showConfirmButton: false,
+      toast: true,
+      position: "top-end",
+    });
+  }
+
+  abrirModal(): void {
+    this.modoEdicion = false;
+    this.formularioProducto.reset();
+    this.formularioProducto.patchValue({ estado: true });
+    this.mostrarModal = true;
+    this.productoSeleccionadoId = undefined;
+  }
+
+  cancelar(): void {
+    this.formularioProducto.reset();
+    this.formularioProducto.patchValue({ estado: true });
+    this.modoEdicion = false;
+    this.productoSeleccionadoId = undefined;
+    this.mostrarModal = false;
+  }
+
+  cambiarEstado(producto: Producto): void {
+    const nuevoEstado = !producto.estado;
+    const accion = nuevoEstado ? "activa" : "desactiva";
+
+    Swal.fire({
+      title: `Estás seguro`,
+      text: `Estas a punto de ${accion}r el producto ${producto.nombre}`,
+      icon: `warning`,
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: `Sí, ${accion}`,
+      cancelButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.productoService
+          .cambiarEstado(producto.id!, nuevoEstado)
+          .subscribe(() => {
+            this.cargarProductos();
+            Swal.fire({
+              title: `Éxito`,
+              text: `El producto fue ${accion}do correctamente`,
+              icon: `success`,
+              timer: 2000,
+              showConfirmButton: false,
+            });
+          });
+      }
+    });
+  }
+
+  volver() {
+    this.router.navigate(["/dashboard"]);
   }
 }
