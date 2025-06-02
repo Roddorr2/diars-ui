8b1b973f6c05c460dc06266bab99139695ccaa89
Roddorr2 <rodricu0703@gmail.com>
2025-06-02 02:44:46 -0500

    Added Subcategoria service to match with controller endpoints

---
+++ src/app/services/subcategoria.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subcategoria } from "../models/subcategoria.model";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SubcategoriaService {
+  private apiUrl = "http://localhost:8080/api/subcategorias";
+
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Subcategoria[]> {
+    return this.http.get<Subcategoria[]>(this.apiUrl);
+  }
+}
