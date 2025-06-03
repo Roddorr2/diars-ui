23ced7160fd7f2d3cbc00c257a2d430cec061d57
Roddorr2 <rodricu0703@gmail.com>
2025-06-03 00:10:21 -0500

    Added some Angular components

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app/app.routes";
 import { authInterceptor } from "./app/auth/auth.interceptor";
+import { importProvidersFrom } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    importProvidersFrom(FormsModule)
   ],
 }).catch((err) => console.error(err));
