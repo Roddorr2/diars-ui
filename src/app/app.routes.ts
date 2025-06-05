a0bee895d58e0c2da9c6a9d13b2b99989697f423
Roddorr2 <rodricu0703@gmail.com>
2025-06-05 13:57:28 -0500

    Added a new path for Categoria component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from "./login/login.component";
 import { DashboardComponent } from "./dashboard.component";
 import { ProductoComponent } from "./components/producto/producto.component";
 import { SubcategoriaComponent } from "./components/subcategoria/subcategoria.component";
+import { CategoriaComponent } from "./components/categoria/categoria.component";
 
 export const routes: Routes = [
   {
@@ -22,5 +23,9 @@ export const routes: Routes = [
   {
     path: "components/subcategoria",
     component: SubcategoriaComponent,
+  },
+  {
+    path: "components/categoria",
+    component: CategoriaComponent,
   },
 ];
