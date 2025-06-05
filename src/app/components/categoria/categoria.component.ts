1cd2cee91ba145cde1952f9f173223c918521215
Roddorr2 <rodricu0703@gmail.com>
2025-06-05 17:06:10 -0500

    Added, modified, implemented all features and UI desing for Categoria entity

---
+++ src/app/components/categoria/categoria.component.ts
@@ -0,0 +1,223 @@
+import { Component, inject, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Categoria } from "./../../models/categoria.model";
+import { Router } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from "@angular/forms";
+import { map, debounceTime, switchMap, first } from "rxjs/operators";
+
+import Swal from "sweetalert2";
+import { CategoriaService } from "../../services/categoria.service";
+@Component({
+  selector: "app-categoria",
+  imports: [ReactiveFormsModule, FormsModule],
+  templateUrl: "./categoria.component.html",
+  styleUrl: "./categoria.component.css",
+})
+export class CategoriaComponent implements OnInit {
+  categorias: Categoria[] = [];
+  categoriasFiltradas: Categoria[] = [];
+  formularioCategoria!: FormGroup;
+  router = inject(Router);
+  searchTerm: string = "";
+
+  paginaActual = 1;
+  itemsPorPagina = 10;
+  totalPaginas = 0;
+  paginasArray: number[] = [];
+  categoriasPaginadas: Categoria[] = [];
+
+  modoEdicion: boolean = false;
+  categoriaSeleccionadaId?: number;
+  mostrarModal: boolean = false;
+
+  categoriaOriginal: Categoria | null = null;
+
+  nombreUnicoValidator(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(300),
+        switchMap((nombre: string) =>
+          this.categoriaService.existePorNombre(nombre)
+        ),
+        map((existe: boolean) => (existe ? { nombreExistente: true } : null)),
+        first()
+      );
+    };
+  }
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.inicializarFormulario();
+    this.cargarCategorias();
+  }
+
+  inicializarFormulario(): void {
+    this.formularioCategoria = this.fb.group({
+      nombre: ["", [Validators.required], this.nombreUnicoValidator()],
+    });
+  }
+
+  cargarCategorias(): void {
+    this.categoriaService.listar().subscribe({
+      next: (categorias) => {
+        this.categorias = categorias;
+        this.categoriasFiltradas = [...categorias];
+        this.paginaActual = 1;
+        this.actualizarPaginacion();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  registrar(): void {
+    if (this.formularioCategoria.invalid) return;
+
+    const categoria: Categoria = {
+      ...this.formularioCategoria.value,
+    };
+    this.categoriaService.registrar(categoria).subscribe(() => {
+      this.cargarCategorias();
+      this.cancelar();
+    });
+
+    Swal.fire({
+      title: "¡Registrado",
+      text: "La categoría se registró correctamente.",
+      icon: "success",
+      timer: 2000,
+      showConfirmButton: false,
+      toast: true,
+      position: "top-end",
+    });
+  }
+
+  editar(categoria: Categoria): void {
+    this.modoEdicion = true;
+    this.categoriaSeleccionadaId = categoria.id;
+    this.mostrarModal = true;
+
+    this.categoriaOriginal = { ...categoria };
+
+    this.formularioCategoria.patchValue({
+      nombre: categoria.nombre,
+    });
+  }
+
+  actualizar(): void {
+    if (this.formularioCategoria.invalid) return;
+
+    const categoriaActualizada: Categoria = {
+      ...this.formularioCategoria.value,
+      categoria: {
+        id: this.formularioCategoria.value.categoria,
+      },
+    };
+
+    if (
+      JSON.stringify(categoriaActualizada) ===
+      JSON.stringify({
+        ...this.categoriaOriginal,
+      })
+    ) {
+      Swal.fire({
+        icon: "info",
+        title: "Sin cambios",
+        text: "No se ha realizado ninguna modificación.",
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+      return;
+    }
+
+    this.categoriaService
+      .actualizar(this.categoriaSeleccionadaId!, categoriaActualizada)
+      .subscribe(() => {
+        this.cargarCategorias();
+        this.cancelar();
+      });
+    Swal.fire({
+      title: "¡Actualizado!",
+      text: "La categoría se actualizó correctamente.",
+      icon: "success",
+      timer: 2000,
+      showConfirmButton: false,
+      toast: true,
+      position: "top-end",
+    });
+  }
+
+  abrirModal(): void {
+    this.modoEdicion = false;
+    this.formularioCategoria.reset();
+    this.mostrarModal = true;
+    this.categoriaSeleccionadaId = undefined;
+  }
+
+  cancelar(): void {
+    this.formularioCategoria.reset();
+    this.modoEdicion = false;
+    this.categoriaSeleccionadaId = undefined;
+    this.mostrarModal = false;
+  }
+
+  filtrarCategorias(): void {
+    const termino = this.searchTerm.trim();
+
+    if (termino === "") {
+      this.cargarCategorias();
+      this.categoriasFiltradas = [...this.categorias];
+      return;
+    } else {
+      this.categoriaService.buscarPorNombre(termino).subscribe({
+        next: (categorias) => {
+          this.categoriasFiltradas = categorias;
+          this.paginaActual = 1;
+          this.actualizarPaginacion();
+        },
+        error: (err) => console.error(err),
+      });
+    }
+  }
+
+  cambiarPagina(pagina: number): void {
+    if (pagina < 1 || pagina > this.totalPaginas) return;
+    this.paginaActual = pagina;
+    this.actualizarPaginacion();
+  }
+
+  actualizarPaginacion(): void {
+    const base = this.categoriasFiltradas;
+
+    this.totalPaginas = Math.ceil(base.length / this.itemsPorPagina);
+    this.paginasArray = Array.from(
+      { length: this.totalPaginas },
+      (_, i) => i + 1
+    );
+
+    const inicio = (this.paginaActual - 1) * this.itemsPorPagina;
+    const fin = inicio + this.itemsPorPagina;
+
+    this.categoriasPaginadas = base.slice(inicio, fin);
+  }
+
+  onItemsPorPaginaChange(): void {
+    this.cambiarPagina(1);
+    this.actualizarPaginacion();
+  }
+
+  volver() {
+    this.router.navigate(["/dashboard"]);
+  }
+}
