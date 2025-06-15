625367f5be0daf760e5fb87fa4b7c53101eabddb
Roddorr2 <rodricu0703@gmail.com>
2025-06-15 16:32:39 -0500

    Refactorized all entity component paths

---
+++ src/app/components/components.routes.ts
@@ -0,0 +1,20 @@
+// src/app/components/components.routes.ts
+import { Routes } from "@angular/router";
+import { ProductoComponent } from "./producto/producto.component";
+import { SubcategoriaComponent } from "./subcategoria/subcategoria.component";
+import { CategoriaComponent } from "./categoria/categoria.component";
+import { SucursalComponent } from "./sucursal/sucursal.component";
+import { CargoComponent } from "./cargo/cargo.component";
+import { ProveedorComponent } from "./proveedor/proveedor.component";
+import { UsuarioComponent } from "./usuario/usuario.component";
+
+// Exportamos un array de Rutas, que será cargado perezosamente por el archivo principal
+export const COMPONENTS_ROUTES: Routes = [
+  { path: "producto", component: ProductoComponent },
+  { path: "subcategoria", component: SubcategoriaComponent },
+  { path: "categoria", component: CategoriaComponent },
+  { path: "sucursal", component: SucursalComponent },
+  { path: "cargo", component: CargoComponent },
+  { path: "proveedor", component: ProveedorComponent },
+  { path: "usuario", component: UsuarioComponent },
+];
