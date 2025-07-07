bfd71e92af4e858bb0a72e89ec1bd1fff3e63ce6
Roddorr2 <rodricu0703@gmail.com>
2025-07-06 23:30:15 -0500

    Added MovimientoAlmacen pipe for tipoAlmacen enum

---
+++ src/app/pipes/tipo-movimiento-almacen.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TipoMovimientoAlmacenPipe } from './tipo-movimiento-almacen.pipe';
+
+describe('TipoMovimientoAlmacenPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TipoMovimientoAlmacenPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
